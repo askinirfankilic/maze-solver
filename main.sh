4bd6dd3b0ed649144568e45292a60bfe50b71160
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2024-09-20 23:56:43 +0300

    initialize project

---
+++ main.sh
@@ -0,0 +1,4 @@
+python3 -m src.main
+# PORT=8888
+# echo 'server opening at http://localhost:$PORT'
+# cd public && python3 -m http.server $PORT
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("hi mark")
+
+
+if __name__ == "__main__":
+    main()
