48e0b27c679ff6e31568d1ef18c1ce8892e3f7a0
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2024-09-21 01:03:57 +0300

    draw a simple line

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+from src.geometry import Line, Point
+from src.window import Window
+
+
 def main():
-    print("hi mark")
+    wnd = Window(500, 500)
+    p1 = Point(250, 250)
+    p2 = Point(250, 150)
+    wnd.draw_line(Line(p1, p2), "red")
+    wnd.wait_for_close()
 
 
 if __name__ == "__main__":
