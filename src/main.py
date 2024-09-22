0cc53532f4abf98160278c74034331164b19df78
Irfan Askin Kilic <kictakimuhabirsincap@gmail.com>
2024-09-22 15:01:17 +0300

    automatize grid generation

--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@ from src.window import Window
 
 
 def main():
-    width = 1000
-    height = 1000
-    wnd = Window(width, height)
-    x1 = 0
-    y1 = 0
     num_rows = 10
     num_cols = 10
-    cell_size_x = width / num_cols
-    cell_size_y = height / num_rows
-    maze = Maze(x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, wnd)
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    wnd = Window(screen_x, screen_y)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, wnd)
     wnd.wait_for_close()
 
 
